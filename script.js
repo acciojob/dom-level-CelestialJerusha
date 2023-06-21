7eec41339d2de13ceca645fd61bfc46cd661cb07
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-06-22 00:38:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function getIdByLevel(level){
+  var element = $("#level");  // 
+  for (var i = 0; i < level; i++){
+    element = $(element).find("li")[0]; // <- the first div in each level is being taken
+  }
+  return element.id;
+}
